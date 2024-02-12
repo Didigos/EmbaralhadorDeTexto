f12ea1d892247958715404d7c34156d55b158f6a
Didigos <diego.didigos@gmail.com>
2024-02-12 15:27:35 -0300

    trabalho finalizado

--- app.js
+++ app.js
@@ -1,60 +1,109 @@
-const resultado = document.querySelector('#resultado')
-const btn_cripto = document.querySelector('#btn_criptografar')
-const btn_descript = document.querySelector('#btn_descriptografar')
-const main2_container = document.querySelector('.button_main')
-const main_textarea = document.querySelector('.main_textarea')
-let texto1 = document.querySelector('#texto1')
-const reset = document.querySelector('#reset')
+const resultado = document.querySelector('#resultado');
+const btn_cripto = document.querySelector('#btn_criptografar');
+const btn_descript = document.querySelector('#btn_descriptografar');
+const main2_container = document.querySelector('.button_main');
+const main_textarea = document.querySelector('#main_textarea');
+let texto1 = document.querySelector('#texto1');
+const reset = document.querySelector('#reset');
+const btn_copy = document.querySelector('.copy_icon');
+const message = document.querySelector('.message')
+const warning = document.querySelector('#warning')
+const warning_text = document.querySelector('#warning_text')
 
-const btn_copy = document.createElement('button')
-
-btn_descript.addEventListener('click', ()=>{
+btn_descript.addEventListener('click', () => {
     descript()
 })
 
-
-btn_cripto.addEventListener('click', ()=>{
+btn_cripto.addEventListener('click', () => {
     encripto()
 })
 
-reset.addEventListener('click', ()=> location.reload())
+texto1.addEventListener('input', function (event) {
+
+    let textoDigitado = event.target.value
+
+    if (temMaiuscula(textoDigitado)) {
+        texto1.classList.add('texto1__warning');
+        btn_cripto.style.background = '#bbbbbb'
+        btn_cripto.setAttribute('disabled',true)
+        warning.style.visibility = 'visible'
+        warning.style.width = 'fit-content'
+        warning_text.textContent = 'Não pode conter letras Maíusculas!'
+
+    } else if (temAcentuacao(textoDigitado)) {
+        texto1.classList.add('texto1__warning');
+        btn_cripto.style.background = '#bbbbbb'
+        btn_cripto.setAttribute('disabled',true)
+        warning.style.visibility = 'visible'
+        warning.style.width = 'fit-content'
+        warning_text.textContent = 'Não pode conter acentuação!'
+    }
+    else {
+        texto1.classList.remove('texto1__warning');
+        btn_cripto.removeAttribute('disabled')
+        btn_cripto.style.background = '#04AA6D'
+        warning.style.width = '25px'
+        warning.style.visibility = 'hidden'
+    }
+
+})
+
+reset.addEventListener('click', () => location.reload())
+
+btn_copy.addEventListener('mouseover', ()=>{
+    message.style.display = 'block'
+})
+
+btn_copy.addEventListener('mouseout', ()=>{
+    message.style.display = 'none'
+    message.style.background = '#3E8ACC'
+    message.textContent = 'Copiar'
+})
+
+btn_copy.addEventListener('click',()=>{
+    copiarTexto()
+    message.style.background = '#04AA6D'
+    message.textContent = 'Copiado!'
+})
 
 function encripto() {
     let textoDigitado = texto1.value.toLowerCase()
     let textoModificado = textoDigitado.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat')
-    
     resultado.style.background = '#ffff'
     resultado.removeAttribute('disabled')
-    resultado.textContent = removeAcenturacoes(textoModificado)
-
-    criarBotaoCopiar()    
+    resultado.textContent = textoModificado
+    btn_copy.style.display = 'block'
 
 }
 
-function descript(){
+function descript() {
     let textoColado = texto1.value
-    let textodescriptografado = textoColado.replace(/enter/g, 'e').replace(/imes/g,'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u')
-    resultado.style.background = '#ffff'
+    let textodescriptografado = textoColado.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u')
+
     resultado.removeAttribute('disabled')
 
     return resultado.textContent = textodescriptografado
-}
-
-function criarBotaoCopiar() {
-    main2_container.appendChild(btn_copy)
-    btn_copy.id = 'copiar'
-    btn_copy.setAttribute('onclick', 'copiarTexto()')
-    btn_copy.textContent = 'copiar texto'
-
 }
 
 function copiarTexto() {
     resultado.select();
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
-    btn_copy.textContent = 'Copiado!'
 }
 
-function removeAcenturacoes(texto){
+function removeAcenturacoes(texto) {
     return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
+
+function temMaiuscula(texto) {
+    return /[A-Z]/.test(texto);
+}
+
+function temAcentuacao(texto) {
+    return /[áàãâéêíóôõúü]/i.test(texto);
+}
+
+function textoDeAviso(){ 
+    warning.style.width = '100px'
+}
+
